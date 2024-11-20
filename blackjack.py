f9b50f5a4200e3d90783144e4fa92df02cf8eae8
Randall <randallhunter2002@gmail.com>
2024-11-20 00:25:04 -0330

    Added buy_chips function

--- blackjack.py
+++ blackjack.py
@@ -34,7 +34,7 @@ def get_card(deck, hand, choose):
 
 # checks if the player draws an ace
 def check_ace(card, hand):
-    print(f"You drew an {card[1]} of {card[0]}")
+    print(f"\nYou drew an {card[1]} of {card[0]}")
     while True:
         choice = input("Choose card value (1 or 11): ")
         if choice == "1" or choice == "11":
@@ -51,37 +51,36 @@ def print_cards(hand):
     for card in hand:
         print(f"{card[1]} of {card[0]}")
 
-# validates the players money and retrives the bet amount
-def get_bet(money):
-    if money < 5:
-        while True:
-            choice = input("Would you like to purchase more chips? (y/n): ")
-            if choice == "y":
-                while True:
-                    try:
-                        money = int(input("Enter chip amount: "))
-                        if money < 5 or money > 1000:
-                            money = 0
+# prompts the user to buy chips
+def buy_chips(money):
+    while True:
+        choice = input("Would you like to purchase more chips? (y/n): ")
+        if choice == "y":
+            while True:
+                try:
+                    while True:
+                        new_money = int(input("Enter chip amount: "))
+                        if new_money < 5 or new_money > 1000:
                             print("Invalid chip amount\n")
                         else:
-                            break
+                            db.write_money(new_money)
+                            print(f"\nMoney: ${new_money}")
+                            return new_money
 
-                    except ValueError:
-                        print("Invalid chip amount, please try again.\n")
+                except ValueError:
+                    print("Invalid chip amount, please try again.\n")
 
-                break
-                    
+        elif choice == "n":
+            print("\nCome back soon!")
+            print("Bye!")
+            sys.exit()
+            break
 
-            elif choice == "n":
-                print("\nCome back soon!")
-                print("Bye!")
-                sys.exit()
-                break
+        else:
+            print("Invalid input, please try again.\n")
 
-            else:
-                print("Invalid input, please try again.")
-
-
+# validates the players money and retrives the bet amount
+def get_bet(money):
     while True:
         try:
             bet_amount = float(input("Bet amount: $"))
@@ -133,26 +132,28 @@ def get_winner(players_hand, dealers_hand, money, bet, blackjack):
     print(f"\nYOUR POINTS:    {players_points}")
     print(f"DEALERS POINTS: {dealers_points}")
     if blackjack:
-        print("\nYou win!")
-        print(f"Money: {round(money + (bet * 1.5), 2)}")
+        print("\nBlackjack!")
+        print("You win!")
+        print(f"Money: ${round(money + (bet * 1.5), 2)}")
         db.write_money(round(money + (bet * 1.5), 2))
     elif players_points > dealers_points and players_points <= 21 or players_points <= 21 and dealers_points > 21:
         print("\nYou win!")
-        print(f"Money: {round(money + bet, 2)}")
+        print(f"Money: ${round(money + bet, 2)}")
         db.write_money(round(money + bet, 2))
     elif players_points < dealers_points and dealers_points <= 21 or players_points > 21:
         print("\nSorry. You lose.")
-        print(f"Money: {round(money - bet, 2)}")
+        print(f"Money: ${round(money - bet, 2)}")
         db.write_money(round(money - bet, 2))
     else:
         print("\nPush.")
+        print(f"Money: ${round(money, 2)}")
+        db.write_money(round(money, 2))
 
 # checks for blackjack
 def check_for_blackjack(players_hand, dealers_hand):
     players_points = get_points(players_hand)
     dealers_points = get_points(dealers_hand)
     if players_points == 21 and players_points != dealers_points:
-        print("\nBlackjack!")
         return True
     else:
         return False
@@ -162,7 +163,9 @@ def main():
     while True:
         money = db.get_money()
         title()
-        print(f"Money: {money}")
+        print(f"Money: ${money}")
+        if money < 5:
+            money = buy_chips(money)
         deck = get_deck()
         bet = get_bet(money)
         db.write_money(money - bet)
